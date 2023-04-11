44dd79b7de00ae89eb61b40f2a046f673b74bcec
Daus <i.daus2210@gmail.com>
2023-04-11 15:15:09 +0800

    login

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.use(express.json())
 app.post('/login', (req, res) => {
   console.log(req.body)
 
-  res.send('login')
+  let result = login(req.body.username, req.body.password)
+
+  res.send(result)
 })
 
 app.get('/', (req, res) => {
@@ -21,6 +23,64 @@ app.get('/bye', (req, res) => {
 app.get('/reg', (req, res) => {
   res.send('Assalamualaikum K')
 })
+
+
+app.post('/register',(req, res)=> {
+
+  let result = register(
+    req.body.username,
+    req.body.password,
+    req.body.name,
+    req.body.email
+  )
+  res.send(result)
+})
+
+function login(reqUsername, reqPassword) {
+  let matchUser = dbUsers.find(
+      user => user.username == reqUsername      //=> what to do with user
+  )
+  if (!matchUser) return "User not found!"
+  if (matchUser.password == reqPassword) {
+      return matchUser
+  } 
+  else
+  {
+      return "Invalid password"
+  }
+  }
+
+  function register(reqUsername, reqPassword, reqName, reqEmail)
+   {
+      dbUsers.push({
+          username: reqUsername,
+          password: reqPassword,
+          name: reqName,
+          email: reqEmail
+      })
+  }
+
+  let dbUsers = [
+    {
+        username: "hazim",
+        password: "123456",
+        name: "zim",
+        email: "hazim00@gmail.com"
+    },
+    {
+        username: "firdaus",
+        password: "654321",
+        name: "Firdaus",
+        email: "firdaus2210@gmail.com"
+    },
+    {
+        username: "afnan",
+        password: "7890",
+        name: "afnan",
+        email: "afnan76@gmail.com"
+    }
+]
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
